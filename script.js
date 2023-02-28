9a35c846a8e1c74782b61c23819d9540f8a0436b
G Devendrababu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-02-28 12:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let inp_box=document.getElementById("evaluatedText");
+let count=0;
+let heading=document.getElementById("wordCount");
+
+
+inp_box.addEventListener("click",()=>{
+    heading.innerText=count;
+})
+
+
+inp_box.addEventListener("keydown",(e)=>{
+
+    if(e.key==" ")
+    {
+        count ++;
+        // console.log("hello");
+    }
+    heading.innerText=count;
+ 
+})
